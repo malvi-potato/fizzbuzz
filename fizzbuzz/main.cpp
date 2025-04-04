712a294b53502cbb702fc55e679ef2ec54df5caa
malvi-potato <malvikarnel@gmail.com>
2025-04-04 10:18:36 +0200

    Add files via upload

---
+++ fizzbuzz/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+int fizzbuzz(int n);
+
+int main()
+{
+        int n;
+    while (n != 0){
+        cin << n;
+        fizzbuzz(n);
+        cout << " "<< n << endl;
+}
+    return 0;
+
+}
+
+int fizzbuzz(int n){
+
+    if (n % 3 = 0)
+
+
+
+    else if (n % 5 = 0)
+
+    else if (n % 3,5)
+
+
+}
